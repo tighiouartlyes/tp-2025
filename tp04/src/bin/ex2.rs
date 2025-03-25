7703bf4fda4989f3ae370b58bab05bf2c15c8262
tighiouartlyes <tighiouartlyes1993@gmail.com>
2025-03-25 13:29:52 +0200

    Update ex2.rs

--- tp04/src/bin/ex2.rs
+++ tp04/src/bin/ex2.rs
@@ -1,5 +1,12 @@
-use std::io;
+use std::io::{self, Write};
 
 fn main() {
-    println!("Hello, world!");
+
+    let mut texte: Box<String> = Box::new(String::new());
+    print!("Entrez un texte : ");
+    io::stdout().flush().unwrap();
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    texte.push_str(&input.trim()); 
+    println!("Le texte saisi est : {}", texte);
 }
